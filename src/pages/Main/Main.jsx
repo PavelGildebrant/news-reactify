a77c1f45ec6f83f9cdbb868a5bfda9ba34d9b021
PavelGildebrant <tnarbedlegasap@gmail.com>
2023-10-15 16:07:56 +0700

    git init

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import NewsBanner from '../../components/NewsBanner/NewsBanner';
+import styles from './styles.module.css';
+import { getNews } from '../../API/apiNews';
+import NewsList from '../../components/NewsList/NewsList';
+const Main = () => {
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await getNews();
+        setNews(response.news);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNews();
+  }, []);
+
+  const [news, setNews] = useState([]);
+  return (
+    <main className={styles.main}>
+      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+
+      <NewsList news={news} />
+    </main>
+  );
+};
+
+export default Main;
