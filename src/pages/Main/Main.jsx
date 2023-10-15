a56aa369433f77cac64d3228e8057fa64d3b5343
PavelGildebrant <tnarbedlegasap@gmail.com>
2023-10-15 19:33:37 +0700

    добавлен скелетон

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,12 +3,15 @@ import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import styles from './styles.module.css';
 import { getNews } from '../../API/apiNews';
 import NewsList from '../../components/NewsList/NewsList';
+import Sceleton from '../../components/Sceleton/Sceleton';
 const Main = () => {
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        setIsLoading(true)
         const response = await getNews();
         setNews(response.news);
+        setIsLoading(false)
       } catch (error) {
         console.log(error);
       }
@@ -17,11 +20,22 @@ const Main = () => {
   }, []);
 
   const [news, setNews] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <main className={styles.main}>
-      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+      {news.length > 0 && !isLoading ? (
+        <NewsBanner item={news[0]} />
+      ) : (
+        <Sceleton type="banner" count={1} />
+      )}
 
-      <NewsList news={news} />
+      {!isLoading ? (
+        <NewsList news={news} />
+      ) : (
+        <Sceleton type="item" count={10} />
+      )}
     </main>
   );
 };
