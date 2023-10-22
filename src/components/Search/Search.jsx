a04878b030d9b9339e749313ce5b2806518a2c57
PavelGildebrant <tnarbedlegasap@gmail.com>
2023-10-22 12:01:35 +0700

    реализован поиск новостей по заголовку с задержкой

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.css';
+const Search = ({keywords, setKeywords}) => {
+  return (
+    <div className={styles.search}>
+      <input
+        onChange={(e) => setKeywords(e.target.value)}
+        type="text"
+        value={keywords}
+        className={styles.input}
+        placeholder='javaScript'
+      />
+    </div>
+  );
+};
+
+export default Search;
